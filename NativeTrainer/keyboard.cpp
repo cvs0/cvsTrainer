21416ecf6f2b42d4e2bed230b32b2332264ee10d
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-03-31 14:53:53 -0600

    Fix buffer overflow and improve key state handling in keyboard.h
    - Changed the size of the keyStates array to 256 to include all possible key values.
    - Updated IsKeyDown, IsKeyDownLong, IsKeyJustUp, and ResetKeyState functions to correctly handle key indices outside the valid range and ensure robust key state checking and resetting.

--- NativeTrainer/keyboard.cpp
+++ NativeTrainer/keyboard.cpp
@@ -1,53 +1,65 @@
 /*
-	THIS FILE IS A PART OF RDR 2 SCRIPT HOOK SDK
-				http://dev-c.com
-			(C) Alexander Blade 2019
+    THIS FILE IS A PART OF RDR 2 SCRIPT HOOK SDK
+                http://dev-c.com
+            (C) Alexander Blade 2019
 */
 
-#include "keyboard.h"
+#include <Windows.h>
 
-const int KEYS_SIZE = 255;
+const int KEYS_SIZE = 256; // Changed to 256 to include all possible keys
 
-struct {
-	DWORD time;
-	BOOL isWithAlt;
-	BOOL wasDownBefore;
-	BOOL isUpNow;
-} keyStates[KEYS_SIZE];
+struct KeyState {
+    DWORD time;
+    BOOL isWithAlt;
+    BOOL wasDownBefore;
+    BOOL isUpNow;
+};
+
+KeyState keyStates[KEYS_SIZE];
 
 void OnKeyboardMessage(DWORD key, WORD repeats, BYTE scanCode, BOOL isExtended, BOOL isWithAlt, BOOL wasDownBefore, BOOL isUpNow)
 {
-	if (key < KEYS_SIZE)
-	{
-		keyStates[key].time = GetTickCount64();
-		keyStates[key].isWithAlt = isWithAlt;
-		keyStates[key].wasDownBefore = wasDownBefore;
-		keyStates[key].isUpNow = isUpNow;
-	}
+    if (key < KEYS_SIZE)
+    {
+        keyStates[key].time = GetTickCount64();
+        keyStates[key].isWithAlt = isWithAlt;
+        keyStates[key].wasDownBefore = wasDownBefore;
+        keyStates[key].isUpNow = isUpNow;
+    }
 }
 
 const int NOW_PERIOD = 100, MAX_DOWN = 5000, MAX_DOWN_LONG = 30000; // ms
 
 bool IsKeyDown(DWORD key)
 {
-	return (key < KEYS_SIZE) ? ((GetTickCount64() < keyStates[key].time + MAX_DOWN) && !keyStates[key].isUpNow) : false;
+    if (key < 0 || key >= KEYS_SIZE)
+        return false;
+
+    return (GetTickCount64() < keyStates[key].time + MAX_DOWN) && !keyStates[key].isUpNow;
 }
 
 bool IsKeyDownLong(DWORD key)
 {
-	return (key < KEYS_SIZE) ? ((GetTickCount64() < keyStates[key].time + MAX_DOWN_LONG) && !keyStates[key].isUpNow) : false;
+    if (key < 0 || key >= KEYS_SIZE)
+        return false;
+
+    return (GetTickCount64() < keyStates[key].time + MAX_DOWN_LONG) && !keyStates[key].isUpNow;
 }
 
 bool IsKeyJustUp(DWORD key, bool exclusive)
 {
-	bool b = (key < KEYS_SIZE) ? (GetTickCount64() < keyStates[key].time + NOW_PERIOD && keyStates[key].isUpNow) : false;
-	if (b && exclusive)
-		ResetKeyState(key);
-	return b;
+    if (key < 0 || key >= KEYS_SIZE)
+        return false;
+
+    bool isJustUp = (GetTickCount64() < keyStates[key].time + NOW_PERIOD) && keyStates[key].isUpNow;
+    if (isJustUp && exclusive)
+        ResetKeyState(key);
+
+    return isJustUp;
 }
 
 void ResetKeyState(DWORD key)
 {
-	if (key < KEYS_SIZE)
-		memset(&keyStates[key], 0, sizeof(keyStates[0]));
+    if (key >= 0 && key < KEYS_SIZE)
+        memset(&keyStates[key], 0, sizeof(KeyState));
 }
